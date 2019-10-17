3757dc2be8bd8d371d74697e7a13e04b0450b3db
Isaac Lozano <109lozanoi@gmail.com>
2019-10-16 22:33:20 -0700

    updated license

--- gvusb2-regs.h
+++ gvusb2-regs.h
@@ -1,4 +1,15 @@
-/* SPDX-License-Identifier: GPL-2.0+ OR BSD-3-Clause */
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-3-Clause */
+/*
+ * Copyright (c) 2019 Isaac Lozano <109lozanoi@gmail.com>
+ *
+ * This code is released using a dual license strategy: BSD/GPL
+ * You can choose the licence that better fits your requirements.
+ *
+ * Released under the terms of 3-clause BSD License
+ * Released under the terms of GNU General Public License Version 2.0
+ *
+ */
+
 #ifndef __GVUSB2_REGS_H__
 #define __GVUSB2_REGS_H__
 
